983ccdd88c947e2440d4b51b78fef35f18778cb3
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-03-22 16:02:55 +0800

    build: project init

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [C3M3] (2024-03-22)
+
+### Build
+
+01 project init ([](https://github.com/Alejandrocsdev/restRaw/commit/))
---
+++ app.js
@@ -0,0 +1,29 @@
+// MODULE
+const http = require('http')
+// SERVER
+const server = http.createServer()
+const host = 'localhost'
+const port = 3000
+server.on('request', requestListener)
+server.listen(port, () => console.log(`http://${host}:${port}`))
+
+function requestListener(request, response) {
+  const url = new URL(request.url, `http://${host}:${port}`)
+  const urlParams = url.searchParams
+  const pathname = url.pathname
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  // GET // root // (/)
+  if (pathname === '/') {
+    response.writeHead(302, { Location: '/restaurants' })
+    response.end('<h1>This is the root</h1>')
+  }
+  // GET // index // (/restaurants)
+  else if (pathname === '/restaurants') {
+    response.end('<h1>This is INDEX page</h1>')
+  }
+  // ERROR - 404
+  else {
+    response.writeHead(404)
+    response.end('<h1>Not Found</h1>')
+  }
+}
